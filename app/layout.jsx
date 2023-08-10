b6b212402c3c13c94651c8c842f8919a9f907d05
Gregorio Cardona <dani12ca12@gmail.com>
2023-08-10 10:25:30 -0500

    promptopia aplicacion

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from '@components/Provider'
+export const metadata = {
+  title: "Promptopia",
+  discover: "Discover & Share AI Prompts",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="es">
+      <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient"/>
+        </div>
+        <main className="app">
+          <Nav/>
+          {children}
+        </main>
+      </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
